9e3430242cfbf0f7d4f13531fc11629c4aff7208
Ubuntu <ubuntu@ip-172-31-3-214.us-west-2.compute.internal>
2013-09-24 09:41:28 +0000

    added web.js, package.json, index.html

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var theIndexfile ="index.html";
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync(theIndexfile,'utf8'));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
