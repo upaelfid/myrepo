1f573ef8a07ecd7ca77be0058b7577e72180b1e2
Ubuntu <ubuntu@ip-172-31-3-214.us-west-2.compute.internal>
2013-10-11 03:41:00 +0000

    deleted some file moved files and new web.js

--- web.js
+++ web.js
@@ -1,17 +1,52 @@
+/*// Define routes for simple SSJS web app. 
+// Writes Coinbase orders to database.
+
+var express = require('express');
+var fs      = require('fs');
+
+var app = express();
+app.set('exterior', __dirname + '/Exterior');
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8080);
+
+
+// Render homepage (note trailing slash): example.com/
+app.get('/', function(request, response) {
+  var data = fs.readFileSync('index.html').toString();
+  response.send(data);
+});
+
+
+// Render exterior (note trailing slash): example.com/
+app.get('/Exterior', function(request, response) {
+  var data = fs.readFileSync('exterior.html').toString();
+  response.send(data);
+});
+
+*/
+
+var express = require('express');
+var app = express.createServer(express.logger());
+var path = require('path');
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'views'))); //  "public" off of current is root
+
+app.listen(8080);
+console.log('Listening on port 8080');
+/*
 var express = require('express');
 var fs = require('fs');
-var theIndexfile ="index.html";
+//var theIndexfile ="index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(theIndexfile,'utf8'));
+  response.send(fs.readFileSync('index.html','utf8'));
 });
 
-app.get('/Exterior', function(request, response) {
-  response.send(fs.readFileSync('exterior.html','utf8'));
-})
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
