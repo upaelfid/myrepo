397efa4aa90f59b20805bbfd8a1e2feeb7fa0688
Ubuntu <ubuntu@ip-172-31-3-214.us-west-2.compute.internal>
2013-10-13 03:51:00 +0000

    updated express and moved around some files

--- web.js
+++ web.js
@@ -1,14 +1,29 @@
-/*// Define routes for simple SSJS web app. 
+/*var express = require('express');
+var fs = require('fs');
+var theIndexfile ="index.html";
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync(theIndexfile,'utf8'));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});*/
+
+
+// Define routes for simple SSJS web app. 
 // Writes Coinbase orders to database.
 
 var express = require('express');
 var fs      = require('fs');
-
+ 
+var port = process.env.PORT || 8080;
 var app = express();
-app.set('exterior', __dirname + '/Exterior');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
-
 
 // Render homepage (note trailing slash): example.com/
 app.get('/', function(request, response) {
@@ -16,36 +31,8 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
+app.use("/img",express.static(__dirname+"/img"));
 
-// Render exterior (note trailing slash): example.com/
-app.get('/Exterior', function(request, response) {
-  var data = fs.readFileSync('exterior.html').toString();
-  response.send(data);
-});
-
-*/
-
-var express = require('express');
-var app = express.createServer(express.logger());
-var path = require('path');
-
-
-app.use(express.static(path.join(__dirname, 'views'))); //  "public" off of current is root
-
-//app.listen(8080);
-//console.log('Listening on port 8080');
-/*
-var express = require('express');
-var fs = require('fs');
-//var theIndexfile ="index.html";
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html','utf8'));
-});
-*/
-
-var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
